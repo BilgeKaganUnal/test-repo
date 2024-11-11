5ad0140f9640ef21762d9bcb814339975786b674
BilgeKaganUnal <bilge.kagan8@gmail.com>
2024-11-11 07:38:54 +0300

    test

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
 // A simple function that adds two numbers
 function add(a: number, b: number): number {
-    return a + b;
+  return a + b;
 }
 
 // A basic interface
 interface Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
 // Creating an object using the Person interface
 const person: Person = {
-    name: "John",
-    age: 30,
+  name: "John",
+  age: 30,
 };
 
 // Logging the results to the console
 console.log("Sum of 5 and 10:", add(5, 10));
 console.log("Person:", person);
 console.log("testing");
+console.log("testing3");
