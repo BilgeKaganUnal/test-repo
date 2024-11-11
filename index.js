6aa57bbd783c17e4aee69d08a65428923bbe8688
BilgeKaganUnal <bilge.kagan8@gmail.com>
2024-11-11 07:06:45 +0300

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+// A simple function that adds two numbers
+function add(a, b) {
+    return a + b;
+}
+// Creating an object using the Person interface
+const person = {
+    name: "John",
+    age: 30,
+};
+// Logging the results to the console
+console.log("Sum of 5 and 10:", add(5, 10));
+console.log("Person:", person);
